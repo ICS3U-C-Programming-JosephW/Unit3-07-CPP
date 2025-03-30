f7af65e4d8cebc701a3bee9d4e07c9fa43781162
JosephWondimagnehu <joseph.wondimagnehu@stu.ocsb.ca.>
2025-03-30 17:43:43 -0400

    Added dating.cpp

---
+++ dating.cpp
@@ -0,0 +1,59 @@
+// Copyright (c) 2025. Joseph Wondimagnehu. All rights reserved.
+//
+// Created by: Joseph Wondimagnehu
+// Date: Mar. 30, 2025
+/* This program checks if a user's age is older than 25
+and younger than 40 to get an approval
+from grandparents to date their grandchild. Adds additional
+checks for validating age inputs. */
+
+/* Include the iostream library for
+input and output functionalities. */
+#include <iostream>
+/* Include the string library to
+adhere to the style guidelines. */
+#include <string>
+
+// Runs the main function.
+int main() {
+    // Declare the string variable for the user's age.
+    std::string ageAsString;
+
+    // Ask for the user's age.
+    std::cout << "You want to date our grandchild? How old are you?\n";
+    // Store the user's input in the string-type age variable.
+    std::cin >> ageAsString;
+
+    // Try to check the validity of the entered age.
+    try {
+        // Declare the integer variable for the user's age.
+        int age;
+        // Attempt to convert the user's age into an integer.
+        age = std::stoi(ageAsString);
+
+        // Check if the user is older than 25 and younger than 40.
+        if ((age > 25) && (age < 40)) {
+            // Display the user is approved of dating the grandchild.
+            std::cout << "\nYou are approved of dating our grandchild.\n";
+        } else if ((age < 0) || (age > 120)) {
+            /* Also, check if the user enters an 
+            invalid age not between 0 and 120. */
+            // Display the user needs to enter a valid age.
+            std::cout << "\nPlease enter a valid age.\n";
+        } else {
+            // Otherwise, they are too young or too old for expectations.
+            // Display the user is not approved of dating the grandchild.
+            std::cout << "\nYou cannot date our grandchild! "
+            << "You must be older than 25 and younger than 40.\n";
+        }
+    }
+    catch (std::invalid_argument) {
+        /* Run if std::stoi could not convert the
+        user's input value to an integer. */
+        // Display to the user that their age is not an integer.
+        std::cout << "\n" << ageAsString << " is not an integer.\n";
+    }
+
+    // Finally, display a message thanking the user for playing.
+    std::cout << "Thanks for playing!";
+}
